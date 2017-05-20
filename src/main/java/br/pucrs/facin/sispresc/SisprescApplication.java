57f8f991181fd53c782dc678c925eced8387ce01
Lucas Caltabiano <Lucas Caltabiano>
2017-05-20 17:06:40 -0300

    Init

---
+++ src/main/java/br/pucrs/facin/sispresc/SisprescApplication.java
@@ -0,0 +1,12 @@
+package br.pucrs.facin.sispresc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisprescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisprescApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('myApp', ['ngRoute', 'ngCookies'])
+        .config(config);
+        //.run(run);
+
+    config.$inject = ['$routeProvider'];
+
+    function config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/default.html',
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+            });
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+    function run($rootScope, $location, $cookies, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookies.getObject('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
+
+})();
